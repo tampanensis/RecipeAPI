0f04ed9d27722528df3600b4dc188c33a1dc511e
Andrii <andrii@upside-services.com>
2019-06-09 21:59:17 +0300

    Add test for superuser

--- app/core/models.py
+++ app/core/models.py
@@ -6,9 +6,18 @@ from django.db import models
 # Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
         return user
 
 
